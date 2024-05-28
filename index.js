60babf9384c5ddb7947bd93eb78ca113870752e5
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2024-05-28 15:49:16 +0100

    add tournament_details functionality

--- index.js
+++ index.js
@@ -12,15 +12,14 @@ window.addEventListener("load", () => {
 
 function Tournament_Organiser() {
     
+    // variables to store REST API endpoints
     var login_details_url = "https://render-sqlite-deploy.onrender.com/login_details/";
+    var tournament_details_url = "https://render-sqlite-deploy.onrender.com/tournament_details/";
 
     // functions to interact with REST API web service
 
     function get_login_details(username, password) {
-        let entered_username = username;
-        console.log("called again");
-        console.log(username);
-        console.log(password);
+
         function onSuccess(data) {
             if (typeof data.data == "undefined") {
                 document.getElementById("username").value = "invalid username entered";
@@ -32,8 +31,38 @@ function Tournament_Organiser() {
                 window.location.href = "home.html";
             }           
         }
+
         let login_details_get_url = login_details_url + username;
+        
         $.ajax(login_details_get_url, { type: "GET", success: onSuccess });        
+    }
+
+    function get_tournament_details() {
+        
+        function onSuccess(data){
+            document.getElementById("tournament_name").value = data.data[0].tournament_name;
+            document.getElementById("tournament_date").value = data.data[0].tournament_date;
+            document.getElementById("location_name").value = data.data[0].location_name;
+            document.getElementById("location_postcode").value = data.data[0].location_postcode;
+        }
+
+        $.ajax(tournament_details_url, { type: "GET", success: onSuccess });
+    }
+
+    function put_save_tournament_details(tournament_name, tournament_date, location_name, location_postcode) {
+        console.log("inner controller function activated");
+        
+        function onSuccess(data){
+            document.getElementById("tournament_name").setAttribute("readonly", true);
+            document.getElementById("tournament_date").setAttribute("readonly", true);
+            document.getElementById("location_name").setAttribute("readonly", true);
+            document.getElementById("location_postcode").setAttribute("readonly", true);
+        }
+
+        let put_tournament_details_url = tournament_details_url + "1";
+        $.ajax(put_tournament_details_url, { type: "PUT", data: {tournament_name: tournament_name, 
+            tournament_date: tournament_date, location_name: location_name, location_postcode:
+            location_postcode}, success: onSuccess });
     }
 
     // functions to interact with controller
@@ -43,4 +72,25 @@ function Tournament_Organiser() {
         let password = document.getElementById("password").value;
         get_login_details(username, password);
     };
+
+    this.populate_tournament_details = function() {
+        get_tournament_details();
+    }    
+
+    this.edit_tournament_details = function() {
+        document.getElementById("tournament_name").removeAttribute("readonly");
+        document.getElementById("tournament_date").removeAttribute("readonly");
+        document.getElementById("location_name").removeAttribute("readonly");
+        document.getElementById("location_postcode").removeAttribute("readonly");
+    }
+
+    this.save_tournament_details = function() {
+        let tournament_name = document.getElementById("tournament_name").value;
+        let tournament_date = document.getElementById("tournament_date").value;
+        let location_name = document.getElementById("location_name").value;
+        let location_postcode = document.getElementById("location_postcode").value;
+
+        put_save_tournament_details(tournament_name, tournament_date, location_name, location_postcode);
+    }
+
 }
